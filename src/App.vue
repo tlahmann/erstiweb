c8fe9d0fa2b91dcf3e30725361a035ac3c039611
tlahmann <tobiaslahmann@gmail.com>
2020-09-15 17:26:48 +0200

    Add navigation/breadcrumbs bar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <nav>
+    Hier steht die aktuelle route
+  </nav>
   <router-view />
 </template>
 
@@ -22,9 +25,20 @@ body,
   font-family: muli, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
   background-color: #9b9b9b;
-  // filter: sepia(60%) hue-rotate(185deg) saturate(250%);
+  // filter: sepia(100%) hue-rotate(185deg) brightness(85%) contrast(90%)
+  // saturate(200%);
+}
+
+nav {
+  width: 100%;
+  height: 38pt;
+  background-color: #f2f2f2;
+  z-index: 1200;
+  -webkit-box-shadow: 0px 4pt 28pt 0px rgba(0, 0, 0, 0.38);
+  -moz-box-shadow: 0px 4pt 28pt 0px rgba(0, 0, 0, 0.38);
+  box-shadow: 0px 4pt 28pt 0px rgba(0, 0, 0, 0.38);
 }
 </style>
