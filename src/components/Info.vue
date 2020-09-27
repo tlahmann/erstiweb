01395e422c9552ddb4a0f683ee1d62b5403009cb
tlahmann <tobiaslahmann@gmail.com>
2020-09-27 16:05:16 +0200

    Style info window

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -3,19 +3,43 @@
     <Titlebar focused="focused" @update-focus="update" />
     <div id="info-content" v-on:click="this.focused = true">
       <div id="side">
-        <input type="checkbox" name="fh-common" value="bla" />
-        <label for="fh-common"> FH Dortmund Allgemein</label><br />
-        <input type="checkbox" name="communicationDesign" value="foo" />
-        <label for="communicationDesign"> Kommunikationsdesign</label><br />
-        <input type="checkbox" name="film-sound" value="bar" />
-        <label for="film-sound"> Film & Sound</label><br />
-        <input type="checkbox" name="object-room" value="this" />
-        <label for="object-room"> Objekt- & Raumdesign</label><br />
-        <input type="checkbox" name="fimography" value="that" />
-        <label for="fimography"> Filmografie</label><br />
+        <p>FH Dortmund</p>
+        <ul>
+          <li>Bachelor</li>
+          <li>Master</li>
+        </ul>
       </div>
       <div id="infos">
-        This is the {{ focused ? "focused" : "unfocused" }} info,
+        <div id="titles">
+          Bla<br />
+          Bla<br />
+          Bla<br />
+          Bla<br />
+          Bla<br />
+        </div>
+        <div id="content">
+          <h3>BA Fotografie</h3>
+          <br />
+          Wenn Du das hier liest, hast Du Dich erfolgreich für den Studiengang
+          Fotografie eingeschrieben. Dazu erst einmal herzlichen Glück-wunsch!
+          Aber was erwartet Dich nun? Das Studium bereitet Dich in erster Linie
+          auf den professionellen Umgang mit einer Fotokamera vor, um
+          individuell gewünschte Projekte und Konzepte umzusetzen. Diese in Form
+          von Ausstellungen, Installationen oder in Buch- und Magazinformen zu
+          sehen, wird Dich unfassbar stolz machen. Nichts steht Dir und Deiner
+          Kamera im Wege, abgesehen vielleicht von der deutschen Bürokratie,
+          aber selbst wenn diese Dich in bestimmten Bereichen einschränkt, wird
+          sie Dir auch in puncto Selbstständigkeit und Datenschutz sehr helfen.
+          Vieles wird Dir dazu während Deines Studiums erklärt, aber auch die
+          Förderung und Forderung Deiner Kreativität wird nicht zu kurz kommen!
+          Denn natürlich ist das Stu-dium, neben den theoretischen und
+          technischen Lehrveranstaltun-gen, darauf ausgelegt, dass Du ganz viel
+          praktisch arbeitest, um aus Dir eine*n professionelle*n Fotograf*in zu
+          machen und Deinen ganz persönlichen Stil zu entwickeln. Damit Dir das
+          gelingt, bietet Dir die FH die Möglichkeit, mit entsprechendem
+          Equipment zu arbeiten, wie zum Beispiel das hauseigene Studio
+          mitzubenutzen oder professionelle Kameras auszuleihen. scrollen ...
+        </div>
       </div>
     </div>
   </div>
@@ -44,7 +68,7 @@ export default defineComponent({
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 #info {
-  background-color: #ffffff;
+  background-color: #e8e8e8;
   border: solid 1pt rgba(0, 0, 0, 10%);
   border-radius: 7pt;
   width: 1280px;
@@ -57,17 +81,55 @@ export default defineComponent({
     display: flex;
     #side {
       background-color: #e8e8e8;
-      flex: 1 1 20%;
-      height: calc(100% - 40pt);
-      padding: 20pt 17pt;
+      flex: 1 1 18.9%;
+      height: calc(100% - 30pt);
+      padding: 10pt 8pt;
+      display: flex;
+      flex-direction: column;
+      font-size: 0.875rem;
+      line-height: 1.125rem;
       input[type="checkbox"] {
         margin: 6.5px 3px;
       }
+      p {
+        margin: 0;
+        color: rgba(36, 36, 36, 0.6);
+        margin-bottom: 6px;
+      }
+      ul {
+        margin: 0;
+        list-style-type: none;
+        padding-left: 1.5rem;
+        li {
+          margin-bottom: 6px;
+        }
+      }
     }
     #infos {
-      background-color: #66bb6a;
       flex: 1 1 80%;
       height: 100%;
+      display: flex;
+      flex-direction: row;
+      #titles {
+        padding: 1em;
+        background-color: white;
+        flex: 1 1 19.2%;
+        height: 100%;
+        border-right: 1px solid #c8c7c8;
+      }
+      #content {
+        h3 {
+          font-size: 1.75rem;
+          line-height: 2.125rem;
+          margin: 0;
+        }
+        padding: 2em;
+        background-color: white;
+        flex: 1 1 61.9%;
+        height: calc(100% - 5rem);
+        font-size: 1.125rem;
+        line-height: 2.125rem;
+      }
     }
   }
 }
